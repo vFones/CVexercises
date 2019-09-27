2d64df10567a8db05dd5a0db5073ce1a2fe0a7a5
Vittorio Fones <vittorio.fones@gmail.com>
2019-09-27 12:34:39 +0200

    changed script behaviour

--- script.sh
+++ script.sh
@@ -1,1 +1,10 @@
-ls | grep -v "opencv.cpp\|CMakeLists.txt\|test.tif\|script.sh" | xargs rm -rf
+#!/bin/sh
+
+g++ -std=c++11 -Wall -Wextra -pedantic $1 \
+	-I /usr/local/include/opencv4 \
+	-L /usr/local/lib/ \
+	-l opencv_core \
+	-l opencv_imgproc \
+	-l opencv_imgcodecs \
+	-l opencv_highgui
+
