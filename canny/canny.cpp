4fd5fda4fcddec48dd1bb0ed383fedc7db412ef1
Vittorio Fones <vittorio.fones@gmail.com>
2019-10-04 19:11:53 +0200

    Canny refactored

--- canny/canny.cpp
+++ canny/canny.cpp
@@ -1,161 +1,149 @@
-#include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
-using namespace std;
-using namespace cv;
+void Canny(cv::Mat &magnitude, cv::Mat &phase, cv::Mat &dst, int t1, int t2);
 
-void suppression(Mat src, Mat thetas, Mat suppressed);
-void histeresis(Mat dst, int ht, int lt);
-
-int main(int argc, char** argv )
+int main(int argc, char **argv)
 {
-  if ( argc != 6 )
+  if(argc < 5)
   {
-    cout << "usage: opencv <Image_Path> <blur mask size> <sigma> <high> <low>" << endl;
+    std::cerr << "usage: opencv <Image_Path> <blur mask size> <sigma> <high> <low>" << std::endl;
     return -1;
   }
-  Mat image;
-  image = imread( argv[1], IMREAD_GRAYSCALE);
-  // opening as greyscale image
+  cv::Mat src = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
 
-  if ( !image.data )
+  if(src.empty())
   {
-    cout << "No image data" << endl;
+    std::cerr << "No image data" << std::endl;
     return -1;
   }
+  cv::imshow("SRC", src);
 
-  imshow("Lenna", image);
-  
-  int size = atoi(argv[2]);
-  float sigma = atof(argv[3]);
-  int canny_lt = atoi(argv[4]);
-  int canny_ht = atoi(argv[5]);
-  
-  cout << "Using kernel size and sigma -> " << size << " " << sigma << endl;
-  Mat blurred;
-  cv::GaussianBlur(image, blurred, cv::Size(size,size), sigma);
+  //applying a Gaussian Blur to smoooothing everything
+  cv::GaussianBlur(src, src, cv::Size(atoi(argv[2]), atoi(argv[2])), atof(argv[3]) );
 
-  Mat gx, gy;
+  //calculating derivates with sobel
+  cv::Mat gx(CV_32F, src.rows, src.cols);
+  cv::Mat gy(CV_32F, src.rows, src.cols);
+  cv::Mat magnitude;
+  cv::Mat magn_dir;
+  cv::Mat dst = src.clone();
 
-  cv::Sobel(blurred, gx, CV_32F, 1, 0);
-  cv::Sobel(blurred, gy, CV_32F, 0, 1);
+  cv::Sobel(src, gx, CV_32F, 1, 0);
+  cv::Sobel(src, gy, CV_32F, 0, 1);
 
-  Mat mag, mag_8U; 
-  cv::magnitude(gx, gy, mag);
-  mag.convertTo(mag_8U, CV_8U);
-  
-  Mat thetas;
-  cv::phase(gx, gy, thetas, true);
-   
-  Mat suppressed;
-  mag_8U.copyTo(suppressed);
-  suppression(mag_8U, thetas, suppressed); 
+  //calculating intensity of an edge and the direction (mag and phase)
+  cv::magnitude(gx, gy, magnitude);
+  cv::phase(gx, gy, magn_dir, true); //true so result in RADIANS
 
-  Mat thresholded(suppressed.size(), CV_8U);
-  suppressed.copyTo(thresholded);
+  //applying Canny edge detector
+  Canny(magnitude, magn_dir, dst, atoi(argv[4]), atoi(argv[5]));
 
-  histeresis(thresholded, canny_ht, canny_lt);
-  imshow("My Canny", thresholded);
-
-  Mat cvcanny(image.size(), CV_8U);
-
-  //true so calculate magnitudo with squares
-  cv::Canny(blurred, cvcanny, canny_lt, canny_ht, 3, true);
-  imshow("OpenCV Canny", cvcanny);
-
-  waitKey(0);
+  cv::imshow("DST", dst);
+  cv::waitKey(0);
   return 0;
 }
 
-void histeresis(Mat dst, int ht, int lt)
+
+void Canny(cv::Mat &magnitude, cv::Mat &phase, cv::Mat &dst, int t1, int t2)
 {
-  vector<Point> strong_edges;
+  std::cout << "Suppressing non maximum edges" << std::endl;
+  magnitude.convertTo(dst, CV_8U);
+
+  //finding edge direction
   for(auto i = 0; i < dst.rows; i++)
   {
     for(auto j = 0; j < dst.cols; j++)
     {
-      if(dst.at<uchar>(i,j) > ht)
+      int edge = cv::saturate_cast<int>(phase.at<float>(i, j)/45.0)%4;
+      int prev = 0, next = 0, curr = 0;
+
+      switch(edge)
+      {
+        case 0:
+        //case edge is horizontal so left and right check
+          if(j-1 >= 0)
+            prev = dst.at<uchar>(i, j-1);
+          if(j+1 < dst.cols)
+            next = dst.at<uchar>(i, j+1);
+        break;
+
+        case 1:
+        //edge is diagonal
+          if(i-1 >= 0 && j-1 >= 0)
+            prev = dst.at<uchar>(i-1, j-1);
+          if(i+1 < dst.rows && j+1 < dst.cols)
+            next = dst.at<uchar>(i+1, j+1);
+        break;
+
+        case 2:
+        // edge is vertical
+          if(i-1 >= 0)
+            prev = dst.at<uchar>(i-1, j);
+          if(i+1 < dst.rows)
+            next = dst.at<uchar>(i+1, j);
+        break;
+
+        case 3:
+        //edge is reverse diagonal
+          if(i-1 >= 0 && j+1 < dst.cols)
+            prev = dst.at<uchar>(i-1, j+1);
+          if(i+1 < dst.rows && j-1 <= 0 )
+            next = dst.at<uchar>(i+1, j-1);
+        break;        
+      }
+      //if current is less than his prev and next pixel then remove it
+      curr = dst.at<uchar>(i,j);
+      if(prev > curr || next > curr)
+        dst.at<uchar>(i,j) = 0;
+    }
+  }
+  
+  std::cout << "Histeresis" << std::endl;
+  // using thresholds for a first binarization
+  // if magnitude are higher than higher threshold then they are strong edges
+  auto strong_edges = std::list<cv::Point>();
+
+  for(auto i = 0; i < dst.rows; i++)
+  {
+    for(auto j = 0; j < dst.cols; j++)
+    {
+      if(dst.at<uchar>(i,j) > t2)
       {
         dst.at<uchar>(i,j) = 255;
-        strong_edges.push_back(Point(i,j));
+        strong_edges.push_back(cv::Point(i,j));
       }
-      else
-        if(dst.at<uchar>(i,j) < lt)
-          dst.at<uchar>(i,j) = 0;
+      else if(dst.at<uchar>(i,j) < t1)
+        dst.at<uchar>(i,j) = 0;
     }
   }
 
+  std::cout << "Second threshold" << std::endl;
   while(strong_edges.size())
-  {
-    Point edge = strong_edges.front();
+  { //check near strong edges if there are value not strong edges make them
+    cv::Point2i edge = strong_edges.front();
+    strong_edges.pop_front();
     for(auto i = -1; i <= 1; i++)
-    {
       for(auto j = -1; j <= 1; j++)
       {
-        Point point(i+edge.x, j+edge.y);
-        if( point.x >= 0 && point.y >= 0 && point.x < dst.rows && point.y < dst.cols )
+        cv::Point point(i+edge.x, i+edge.y);
+        if(point.x >= 0 && point.y >= 0 && point.x <= dst.rows && point.y <= dst.cols)
         {
-          if( dst.at<uchar>(point.x, point.y) != 0 && dst.at<uchar>(point.x, point.y) != 255 )
+          auto value = dst.at<uchar>(point.x, point.y);
+          if(value != 255 && value != 0)
           {
             dst.at<uchar>(point.x, point.y) = 255;
             strong_edges.push_back(point);
           }
         }
       }
-    }
-    strong_edges.pop_back();
   }
 
-  for(auto it = dst.begin<uchar>(); it < dst.end<uchar>(); it++)
-    if(*it != 0 && *it != 255)
-      *it=0;
+  //since there are no more strong edges normalize every others
+  for(auto i = 0; i < dst.rows; i++)
+    for(auto j = 0; j < dst.cols; j++)
+      if(dst.at<uchar>(i,j) != 0 && dst.at<uchar>(i,j) != 255 )
+        dst.at<uchar>(i,j) = 0;
 
-}
-
-void suppression(Mat src, Mat thetas, Mat suppressed)
-{
-  cout << "suppressing" << endl;
-  vector<int> vec;
-  for(auto i=0;i<src.rows;i++)
-  { 
-    for(auto j=0;j<src.cols;j++)
-    {
-      int edges = saturate_cast<int>(thetas.at<float>(i,j)/45.0)%4;
-      int prev = 0, next = 0, magn = 0;
-      
-      switch(edges)
-      {
-        case 0:
-          if(j-1 >=0)
-            prev = src.at<uchar>(i,j-1);
-          if(j+1 < src.cols)
-            next = src.at<uchar>(i,j+1);
-        break;
-        case 1:
-          if(i-1>=0 && j-1 >= 0)
-            prev = src.at<uchar>(i-1,j-1);
-          if(i+1 < src.rows && j+1 < src.cols)
-            next = src.at<uchar>(i+1,j+1);
-        break;
-        case 2:
-          if(i-1 >= 0)
-           prev = src.at<uchar>(i-1,j);
-          if(i+1 < src.cols)
-           next = src.at<uchar>(i+1,j);
-        break;
-        case 3:
-          if(i-1 >= 0 && j+1 < src.cols)
-            prev = src.at<uchar>(i-1,j+1);
-          if(i+1 < src.rows && j-1 >= 0)
-            next = src.at<uchar>(i+1, j-1);
-        break;
-      }
-
-      magn = src.at<uchar>(i,j);
-      
-      if(prev > magn || next > magn)
-       suppressed.at<uchar>(i,j)=0;
-    }
-  }
-  cout << "suppressed" << endl;
+  std::cout << "Canny applyied" << std::endl;
 }
