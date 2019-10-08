67b9c68ba0643a5e074ca7c56ec5c90ac5b37444
Vittorio Fones <vittorio.fones@gmail.com>
2019-10-08 20:30:38 +0200

    updated hough for circles

--- houghCircles/hc.cpp
+++ houghCircles/hc.cpp
@@ -1,117 +1,94 @@
 #include <iostream>
-#include <math.h>
 #include <opencv2/opencv.hpp>
 
-void MyHoughCircles(cv::Mat &src, cv::Mat &dst, int minradius, int maxradius, int threshold);
+cv::Mat houghCircle(cv::Mat &canny, cv::Mat &src, int minrad, int maxrad, int threshold);
+
 
 int main(int argc, char **argv)
 {
-  if(argc != 5)
+  if(argc < 2)
   {
-    std::cerr << "less args" << std::endl;
-    exit(-1);
+    std::cerr << "exe, path, min radius, max one, votes threshold" << std::endl;
+    exit(EXIT_FAILURE);
   }
-  
-  cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
-  cv::Mat img_gs = cv::imread(argv[1], cv::IMREAD_GRAYSCALE);
+  cv::Mat src = cv::imread(argv[1], 0);
+  if(src.empty())
+  {
+    exit(EXIT_FAILURE);
+  }
+  cv::imshow("SRC", src);
 
-  if( !image.data )
-  {
-    std::cerr<< "error" << std::endl;
-    exit(-1);
-  }
-  
+  //making a gaussian blur before canny
+  cv::Mat blur;
+  cv::GaussianBlur(src, blur, cv::Size(5,5), 2.4);
+  cv::Mat canny;
+  cv::Canny(blur, canny, 100, 150);
+  cv::imshow("Canny", canny);
+
   int minradius = atoi(argv[2]);
   int maxradius = atoi(argv[3]);
   int threshold = atoi(argv[4]);
   
-  cv::Mat blurred;
-  cv::Mat cannyed;
-  cv::Mat dst;
-  std::cout << "Gauss" << std::endl;
-  cv::GaussianBlur(img_gs, blurred, cv::Size(5,5), 4);
-  std::cout << "Canny" << std::endl;
-  cv::Canny(blurred, cannyed, 100, 200, 3);
-  
-  
-  cv::imshow("Orig", image);
-  cv::imshow("canny", cannyed);
-  cv::imshow("blurr", blurred);
-  
-  //cv::cvtColor(image, dst, cv::COLOR_GRAY2BGR);
-  image.copyTo(dst);
-  std::cout << "Hough with min R:" << minradius;
-  std:: cout << ", max R: " << maxradius;
-  std::cout << ", threshold value: " << threshold << std::endl;
+  std::cout << "threshold " << threshold << std::endl;
 
-  MyHoughCircles(cannyed, dst, minradius, maxradius, threshold);
-  std::cout << "Fin" << std::endl;
-  cv::imshow("DST", dst);
+  cv::Mat hough = houghCircle(canny, src, minradius, maxradius, threshold);
+  
+  cv::imshow("Circles", hough);
   
   cv::waitKey(0);
+
   return 0;
 }
 
 
-void MyHoughCircles(cv::Mat &src, cv::Mat &dst, int minradius, int maxradius, int threshold)
+cv::Mat houghCircle(cv::Mat &canny, cv::Mat &src, int minrad, int maxrad, int threshold)
 {
-  int size[] = {src.rows, src.cols, (maxradius-minradius)+1};
-  
+  //creating a 3D matrix with size of A and B (polar coords) and the Radius of circles
+  int size[] = {src.rows, src.cols, (maxrad-minrad)+1};
   cv::Mat acc(3, size, cv::IMREAD_GRAYSCALE, cv::Scalar(0));
 
-  std::cout << "applying hough" << std::endl;
+  cv::Mat hough = src.clone();
 
-  //#pragma omp for
+  cv::cvtColor(hough, hough,cv::COLOR_GRAY2BGR);
+
+
+  //start voting
   for(int i = 0; i < src.rows; i++)
   {
     for(int j = 0; j < src.cols; j++)
     {
-      if(src.at<uchar>(i, j) >= 250)
+      if(canny.at<uchar>(i,j) != 0)
       {
-        for(int r = minradius; r < maxradius; r++)
+        for(int r = minrad; r < maxrad; r++)
         {
           for(int t = 0; t < 360; t++)
           {
-            int a = round( i - r*(cos(t * M_PI/180) ));
-            int b = round( j - r*(sin(t * M_PI/180) ));
-            if( a >= 0 && b >= 0 && a < src.cols && b < src.rows )
-              acc.at<uchar>(a, b, r-minradius)++;
+            int a = cvRound(i - r * (cos(t * CV_PI/180)));
+            int b = cvRound(j - r * (sin(t * CV_PI/180)));
+            if(a >= 0 && a < src.rows && b >= 0 && b < src.cols)
+              acc.at<uchar>(a, b, r-minrad)++;
           }
         }
       }
     }
   }
 
-  std::cout << acc.size() << "Size acc" << std::endl;
-  
-  std::vector<cv::Vec3f> cerchi;
-  std::cout << "Pushing in vector" << std::endl;
-  
-  //#pragma omp for
-  
+  std::cerr << "Accumulator created" << std::endl;
+
+  //if any of the votes are enough big than write down to hough matrix and return
   for(int x = 0; x < src.rows; x++)
   {
     for(int y = 0; y < src.cols; y++)
     {
-      for(int r = 0; r < maxradius-minradius; r++)
+      for(int r = 0; r < maxrad-minrad; r++)
       {
         if(acc.at<uchar>(x, y, r) > threshold)
-        {
-          cv::Vec3f c(x, y, r+minradius);
-          cerchi.push_back(c);
-        }
+          cv::circle(hough, cv::Point(y, x), r + minrad, cv::Scalar(0, 0, 255), 1);
       }
     }
   }
+
+  std::cout << "fin." << std::endl;
   
-  std::cout << "Circles: " << cerchi.size() << std::endl;
-  
-  //#pragma omp for
-  for(int i = 0; i < cerchi.size(); i++)
-  {
-    cv::Point c(cvRound(cerchi[i][1]), cvRound(cerchi[i][0]));
-    int r = cvRound(cerchi[i][2]);
-    cv::circle(dst, c, r, cv::Scalar(0, 0, 255), 2);
-  }
-  return;
+  return hough;
 }
