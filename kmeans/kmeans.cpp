e865f58fe2693702cadd2c026b89d293d69d88a6
Vittorio Fones <vittorio.fones@gmail.com>
2019-10-11 11:21:12 +0200

    Updated kmeans with random centroids

--- kmeans/kmeans.cpp
+++ kmeans/kmeans.cpp
@@ -1,158 +1,140 @@
 #include <iostream>
+#include <random>
 #include <opencv2/opencv.hpp>
 
+double distance(cv::Vec3b &a, cv::Vec3b &b);
+int whichCluster(std::vector<cv::Vec3b> &centroids, cv::Vec3b &pixel);
+void updateCentroids(cv::Mat src, cv::Mat &clusters, std::vector<cv::Vec3b> &centroids);
 
-void kmeans(cv::Mat &src, cv::Mat &cluster, std::vector<cv::Vec3b> &centroid, int k, int max_iter);
+cv::Mat kmeans(cv::Mat src, int clusterSize, int maxIterations);
 
 int main(int argc, char **argv)
 {
-  std::srand(std::time(nullptr));
   if(argc < 4)
   {
-    std::cerr << "Less args" << std::endl;
+    std::cerr << "exe, path, k, iterations" << std::endl;
     exit(EXIT_FAILURE);
   }
   cv::Mat src = cv::imread(argv[1], cv::IMREAD_ANYCOLOR);
-  if(!src.data)
+  
+  if(src.empty())
   {
-    std::cerr << "Image error" << std::endl;
+    std::cerr << "Image not valid" << std::endl;
     exit(EXIT_FAILURE);
   }
-  cv::imshow("Input image", src);
-
   int k = atoi(argv[2]);
-  int iterations = atoi(argv[3]);
+  int maxIt = atoi(argv[3]);
   
-  //cv::resize(src,src,src.size()/3);
-  
+  cv::Mat dst = kmeans(src, k, maxIt);
 
-  cv::Mat dst(src.size(), src.type());
-  cv::Mat clusters(src.size(), CV_8U, cv::Scalar(0));
-  std::vector<cv::Vec3b> centroid(k);
-
-  kmeans(src, clusters, centroid, k, iterations);
-  std::cout << "Fin." << std::endl;
-
-  for (int i=0; i<clusters.rows; i++) {
-        for (int j=0; j<clusters.cols; j++) {
-            int h = clusters.at<uchar>(i,j);
-            dst.at<cv::Vec3b>(i,j) = centroid[h];
-        }
-    }
-  
-
-  cv::imshow("Clustered", dst);
-
-
+  cv::imshow("SRC", src);
+  cv::imshow("Clusters", dst);
 
   cv::waitKey(0);
   return 0;
 }
 
 
-float distBGR(cv::Vec3b &a, cv::Vec3b &b)
+cv::Mat kmeans(cv::Mat src, int clusterSize, int maxIterations)
 {
-  float sum = 0.0;
-  for(int i = 0; i < 3; i++)
-    sum += std::pow(a[i]-b[i], 2);
-  sum = std::sqrt(sum);
-  return sum;
-}
+  int it = 0;
+  cv::Mat clusters(src.size(), CV_8U, cv::Scalar(0));
+  std::vector<cv::Vec3b> centroids(clusterSize);
 
-int PixelInWhichCluster(std::vector<cv::Vec3b> &cluster, cv::Vec3b &pixel)
-{
-  float min = distBGR(cluster.at(0), pixel);
-  int clusterindex = 0;
+  //randomize
+  std::random_device rd;
+  std::mt19937 mt(rd());
+  std::uniform_int_distribution<> dist(0, src.rows);
   
-  for(int h = 1; h < cluster.size(); h++)
+  //getting random pixels and using them as centroids for my clusters 
+  for(int i = 0; i < clusterSize; i++)
   {
-    float distance = distBGR(cluster.at(h), pixel);
-    if( distance < min )
-    {
-      min = distance;
-      clusterindex = h;
-    }
+    int x = dist(mt);
+    int y = dist(mt);
+    centroids.at(i) = src.at<cv::Vec3b>(x, y);
+    clusters.at<uchar>(x, y) = i;
+    std::cout << centroids.at(i) << " " << std::endl;
   }
 
-  return clusterindex;
+  updateCentroids(src, clusters, centroids);
+  std::cout << "Starting iterations" << std::endl;
+
+  for(it = 0; it < maxIterations; it++)
+  {
+    for(auto i = 0; i < src.rows; i++)
+      for(auto j = 0; j < src.cols; j++)
+        clusters.at<uchar>(i, j) = whichCluster(centroids, src.at<cv::Vec3b>(i, j));
+        //look up for a suitable cluster for that pixel and add that
+    
+    //update centroids
+    updateCentroids(src, clusters, centroids);
+  }
+  std::cout << "Fin: " << it << std::endl;
+
+  cv::Mat dst(src.size(), src.type());
+
+  for(int i = 0; i < clusters.rows; i++)
+    for(int j = 0; j < clusters.cols; j++)
+      dst.at<cv::Vec3b>(i, j) = centroids.at(clusters.at<uchar>(i, j));
+      //coloring image with clusters position info using centroids color
+
+  return dst;
 }
 
 
-void updateCentroid(cv::Mat &src, cv::Mat &clusters, std::vector<cv::Vec3b> &centroid)
+double distance(cv::Vec3b &a, cv::Vec3b &b)
 {
-  std::vector<cv::Vec3d> sums(centroid.size(), cv::Vec3d(0,0,0));
-  std::vector<int> counts(centroid.size(), 0);
+  double sum = 0.0;
+  //calculate Euclidian distance for two RGB pixels
+  for(auto i = 0; i < 3; i++)
+    sum += std::pow(a[i] - b[i], 2);
+  return std::sqrt(sum);
+}
 
-  for (int i=0; i<clusters.rows; i++)
+int whichCluster(std::vector<cv::Vec3b> &centroids, cv::Vec3b &pixel)
+{
+  //search for a suitable cluster for a pixel
+  double min = distance(centroids.at(0), pixel);
+  int k = 0;
+
+  for(int i = 1; i < (int)centroids.size(); i++)
   {
-    for (int j=0; j<clusters.cols; j++)
+    double dist = distance(centroids.at(i), pixel);
+    if( dist < min )
     {
-      int h = clusters.at<uchar>(i,j);
-      cv::Vec3b pixel = src.at<cv::Vec3b>(i,j);
-      sums[h] += pixel;
-      counts[h]++;
+      min = dist;
+      k = i;
     }
   }
-
-  for (int k=0; k<centroid.size(); k++)
-  {
-    for (int i=0; i<3; i++)
-    {
-      double value = counts[i] == 0 ? 0 : (1.0*sums[k][i])/(1.0*counts[k]);
-      centroid[k][i] = cv::saturate_cast<uchar>(value);
-    }
-  }
-  //std::cout << "updated centroid" << std::endl;
+  //minimum distantiated one will be returned as index
+  return k;
 }
 
 
-
-void kmeans(cv::Mat &src, cv::Mat &cluster, std::vector<cv::Vec3b> &centroid, int k, int max_iter)
+void updateCentroids(cv::Mat src, cv::Mat &clusters, std::vector<cv::Vec3b> &centroids)
 {
-  std::cerr << "starting kmeans" << std::endl;
+  std::vector<cv::Vec3d> sum(centroids.size(), cv::Vec3b(0,0,0));
+  std::vector<int> pixels(centroids.size(), 0);
 
-/*
-  for (int i=0; i<cluster.rows; i++)
+  //sum every pixels
+  for(auto i = 0; i < clusters.rows; i++)
   {
-    for (int j=0; j<cluster.cols; j++)
+    for(auto j = 0; j < clusters.cols; j++)
     {
-      int c = std::rand() % k;
-      cluster.at<uchar>(i,j) = cv::saturate_cast<uchar>(c);
+      int k = clusters.at<uchar>(i, j);
+      sum.at(k) += src.at<cv::Vec3b>(i,j);
+      pixels.at(k)++;
     }
   }
-*/
-  
-  for( auto i = 0; i < k; i++)
+
+  //if pixels sum is different from 0 start dividing and update centroids
+  for(auto k = 0; k < (int) centroids.size(); k++)
   {
-    int x = rand()%src.rows;
-    int y = rand()%src.cols;
-    centroid.at(i) = src.at<cv::Vec3b>(x, y);
-
-    cluster.at<uchar>(x, y) = i;
-  }
-
-
-  std::cerr << " first touch" << std::endl;
-
-  //updateCentroid(src, cluster, centroid);
-
-  for(int h = 0; h < max_iter; h++)
-  {
-
-    for(int i = 0; i < src.rows; i++)
+    for(auto i = 0; i < 3; i++)
     {
-      for(int j = 0; j < src.cols; j++)
-      {
-        cv::Vec3b pixel = src.at<cv::Vec3b>(i, j);
-        int index = PixelInWhichCluster(centroid, pixel);
-
-        //dst.at<cv::Vec3b>(i,j) = centroid.at(index);
-        
-        cluster.at<uchar>(i, j) = index;
-      }
+      double value = pixels.at(k) == 0 ? 0 : (1.0*sum.at(k)[i])/(1.0*pixels.at(k));
+      centroids.at(k)[i] = cv::saturate_cast<uchar>(value);
+      //using new mean for centroid info
     }
-
-    updateCentroid(src, cluster, centroid);
   }
-  return;
 }
